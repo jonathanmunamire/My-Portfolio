925346c588778439804497f33171a54c0233fde1
jonathanmunamire <jonathanmunamire@gmail.com>
2022-11-12 16:49:29 +0200

    Styling About Section

--- src/index.js
+++ src/index.js
@@ -2,7 +2,13 @@ import _ from 'lodash';
 import './style.css';
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-
+const scrollHeader = () =>{
+    const header = document.getElementById('header')
+    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+    this.scrollY >= 50 ? header.classList.add('scroll-header') 
+                       : header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
 
 /*=============== SERVICES MODAL ===============*/
 
